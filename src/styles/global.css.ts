de5655a335f5ecdb5159449560fa4319b49d9b34
Allan Carvalho <allandcarv@gmail.com>
2025-10-30 14:08:47 +0100

    feat: added global style

--- src/styles/global.css.ts
+++ src/styles/global.css.ts
@@ -1,6 +1,39 @@
 import { globalStyle } from '@vanilla-extract/css';
 
 globalStyle('*', {
+  boxSizing: 'border-box',
   margin: 0,
   padding: 0,
+});
+
+globalStyle('html, body', {
+  width: ['100vw', '100dvw'],
+  height: '100%',
+  minHeight: ['100vh', '100dvh'],
+});
+
+globalStyle('#root', {
+  minHeight: '100%',
+});
+
+globalStyle('a', {
+  textDecoration: 'none',
+  color: 'inherit',
+});
+
+globalStyle('button', {
+  border: 'none',
+  background: 'none',
+  cursor: 'pointer',
+  color: 'inherit',
+  font: 'inherit',
+});
+
+globalStyle('input, textarea, select', {
+  color: 'inherit',
+  font: 'inherit',
+});
+
+globalStyle('p, h1, h2, h3, h4, h5, h6', {
+  overflowWrap: 'break-word',
 });
